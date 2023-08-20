1a0b2569c56106a2c32095243ddc33ccb0fa59b9
TheWass <lord.d35ruct0@gmail.com>
2023-08-20 11:26:24 -0400

    Version test2

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-htmlVersion.innerHTML = "x.x.x";
+htmlVersion.innerHTML = '0.0.2';
 
 // The click event on the pop up notification
 document.getElementById('reload').addEventListener('click', function(){
